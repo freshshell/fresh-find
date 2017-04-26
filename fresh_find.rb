9675e949a24b040b9a251306b4dabaf315672510
Odin Dutton <odindutton@gmail.com>
2017-04-26 19:41:28 +1000

    Introduce module

--- fresh_find.rb
+++ fresh_find.rb
@@ -1,22 +1,31 @@
 require 'octokit'
 
-def access_token
-  ENV.fetch 'ACCESS_TOKEN'
+module FreshFind
+  def self.access_token
+    ENV.fetch 'ACCESS_TOKEN'
+  end
+
+  def self.client
+    @client ||= Octokit::Client.new access_token: access_token
+  end
+
+  def self.items
+    @items ||= client.search_code("fresh filename:freshrc", per_page: 100).items
+  end
+
+  def self.call
+    items.each do |item|
+      content = Base64.decode64 client.contents(item.repository.full_name, path: item.path).content
+      lines = content.lines.select { |line| line.match /^\s*fresh/ }.map &:strip
+      lines.each do |line|
+        p line
+      end
+      # TODO: check these lines work and add them to the fresh directory
+      #
+      # NOTE: these lines contain "fresh-options" which means we probably need to
+      # parse the file with fresh to add any options.
+    end
+  end
 end
 
-def client
-  @client ||= Octokit::Client.new access_token: access_token
-end
-
-def items
-  @items ||= client.search_code("fresh filename:freshrc", per_page: 100).items
-end
-
-items.each do |item|
-  content = Base64.decode64 client.contents(item.repository.full_name, path: item.path).content
-  lines = content.lines.select { |line| line.match /^\s*fresh/ }.map &:strip
-  # TODO: check these lines work and add them to the fresh directory
-  #
-  # NOTE: these lines contain "fresh-options" which means we probably need to
-  # parse the file with fresh to add any options.
-end
+FreshFind.()
